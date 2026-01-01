f6a933a7505393a32171a2bf9ad3307101f7b08f
Galaxy0 <zyl725000@outlook.com>
2026-01-02 01:46:14 +0800

    第二次

---
+++ helloWord/m_packet/n2.py
@@ -0,0 +1,7 @@
+# From: ZYL
+# Time: 2026-01-02  01:45:08
+# Mail: 2790299631@qq.com
+
+"""
+nonononono
+"""
--- helloWord/train.py
+++
@@ -1,7 +0,0 @@
-# From: ZYL
-# Time: 2026-01-02  01:06:14
-# Mail: 2790299631@qq.com
-
-a = 1
-b = 2
-print(a)
